31f9f113f8d12c4a44438b3465cd367f8472a5f2
TokyFy <tokyfydev@gmail.com>
2025-08-12 14:35:44 +0300

    Add permited host

--- index.rb
+++ index.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, 8080
 
+use Rack::Protection::HostAuthorization, permitted_hosts: [
+  'ngnix'
+]
+
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
