43845d77c110034bc8cd4b5acbf6fa14a3a201d1
TokyFy <tokyfydev@gmail.com>
2025-08-12 14:53:23 +0300

    idk

--- index.rb
+++ index.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
 
-disable :protection
-
 set :bind, '0.0.0.0'
 set :port, 8080
 
+set :protection, except: :host_authorization
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
