38d779259b1ba73f789f998fb30634cbd8f47ab4
TokyFy <tokyfydev@gmail.com>
2025-08-12 14:42:18 +0300

    disable protection

--- index.rb
+++ index.rb
@@ -3,10 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, 8080
 
-use Rack::Protection::HostAuthorization, permitted_hosts: [
-  'localhost:8080',       # direct test on host
-  'meine:8080'          # container name in Docker network
-]
+disable :protection
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
